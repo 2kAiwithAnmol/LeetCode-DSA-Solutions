581f2dfe733c88fc8804bc6f35ca455361efe91d
2kAiwithAnmol <2k25aiml2212217@gmail.com>
2025-04-25 01:51:46 +0530

    Added To Lower Case Problem with Solution

---
+++ toLowerCase.js
@@ -0,0 +1,6 @@
+var toLowerCase = function(s) {
+    // Convert the input string 's' to all lowercase letters
+    let newStr = s.toLowerCase();
+    // Return the lowercase string
+    return newStr;    
+};
